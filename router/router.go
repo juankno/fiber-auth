3a077c8a62a727d5d3c6f611fdecaae2d96d2bd4
Juankno <juanes0202@gmail.com>
2025-03-30 22:10:36 -0500

    feat: agregar rutas para gestión de libros y autenticación en el enrutador

--- router/router.go
+++ router/router.go
@@ -1,1 +1,27 @@
 package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/juankno/fiber-auth/controller"
+	"github.com/juankno/fiber-auth/middleware"
+)
+
+func SetupRoutes(app *fiber.App) {
+
+	api := app.Group("/api")
+
+	// Book
+	book := api.Group("/books")
+	book.Get("/", controller.GetBooks)
+	book.Get("/:id", controller.GetBook)
+
+	book.Use(middleware.JWTProtected)
+	book.Post("/", controller.CreateBook)
+	book.Patch("/:id", controller.UpdateBook)
+	book.Delete("/:id", controller.DeleteBook)
+
+	// Auth
+	auth := api.Group("/auth")
+	auth.Post("/login", controller.Login)
+	auth.Post("/register", controller.Register)
+}
