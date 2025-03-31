400e30710f6965dce4372ce3234fec2c5f326105
Juankno <juanes0202@gmail.com>
2025-03-30 22:10:42 -0500

    feat: agregar funciones para gestionar libros en el controlador

--- controller/book.go
+++ controller/book.go
@@ -1,1 +1,28 @@
 package controller
+
+import "github.com/gofiber/fiber/v2"
+
+type bookRequest struct {
+	Title  string `json:"title" validate:"required"`
+	Author string `json:"author" validate:"required"`
+}
+
+func GetBooks(c *fiber.Ctx) error {
+	//
+}
+
+func GetBook(c *fiber.Ctx) error {
+	//
+}
+
+func CreateBook(c *fiber.Ctx) error {
+	//
+}
+
+func UpdateBook(c *fiber.Ctx) error {
+	//
+}
+
+func DeleteBook(c *fiber.Ctx) error {
+	//
+}
