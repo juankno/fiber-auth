e2217c64ea382b3539ac1ca1282b4abf260217bb
Juankno <juanes0202@gmail.com>
2025-03-30 22:36:52 -0500

    feat: implementar funciones para obtener, crear, actualizar y eliminar libros en el controlador

--- controller/book.go
+++ controller/book.go
@@ -1,6 +1,10 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/juankno/fiber-auth/database"
+	"github.com/juankno/fiber-auth/model"
+)
 
 type bookRequest struct {
 	Title  string `json:"title" validate:"required"`
@@ -8,21 +12,49 @@ type bookRequest struct {
 }
 
 func GetBooks(c *fiber.Ctx) error {
-	//
+	return c.Status(200).JSON(fiber.Map{
+		"message": "All books",
+	})
 }
 
 func GetBook(c *fiber.Ctx) error {
-	//
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Get book",
+	})
 }
 
 func CreateBook(c *fiber.Ctx) error {
-	//
+	var req bookRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	book := model.Book{
+		Title:  req.Title,
+		Author: req.Author,
+	}
+
+	res := database.DB.Create(&book)
+
+	if res.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": res.Error.Error(),
+		})
+	}
+	return c.Status(201).JSON(fiber.Map{
+		"message": "Book created",
+	})
 }
 
 func UpdateBook(c *fiber.Ctx) error {
-	//
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Book created",
+	})
 }
 
 func DeleteBook(c *fiber.Ctx) error {
-	//
+	return c.Status(204).JSON(fiber.Map{})
 }
