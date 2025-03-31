a2502528a511f954a479cf9d76043636fa35a7e4
Juankno <juanes0202@gmail.com>
2025-03-30 21:30:49 -0500

    feat: agregar funciones de registro y inicio de sesión en el controlador de autenticación

--- controller/auth.go
+++ controller/auth.go
@@ -1,1 +1,66 @@
 package controller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/juankno/fiber-auth/database"
+	"github.com/juankno/fiber-auth/model"
+	"github.com/juankno/fiber-auth/utils"
+)
+
+type authRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Register(c *fiber.Ctx) error {
+	var req authRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	user := model.User{
+		Email:        req.Email,
+		PasswordHash: utils.GeneratePassword(req.Password),
+	}
+	res := database.DB.Create(&user)
+	if res.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": res.Error.Error(),
+		})
+	}
+	return c.Status(201).JSON(fiber.Map{
+		"message": "user created",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	var req authRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	var user model.User
+	res := database.DB.Where("email = ?", req.Email).First(&user)
+	if res.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+	if !utils.ComparePassword(user.PasswordHash, req.Password) {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	token, err := utils.GenerateToken(user.ID)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"token": token,
+	})
+}
