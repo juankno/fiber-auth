5cd445f1f97bbb60408ebe3b2f6cb92173268dd6
Juankno <juanes0202@gmail.com>
2025-03-30 22:58:13 -0500

    feat: mejorar manejo de errores en las funciones de gestión de libros

--- controller/book.go
+++ controller/book.go
@@ -12,23 +12,34 @@ type bookRequest struct {
 }
 
 func GetBooks(c *fiber.Ctx) error {
-	return c.Status(200).JSON(fiber.Map{
-		"message": "All books",
-	})
+
+	var books []model.Book
+
+	if err := database.DB.Find(&books).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Error retrieving books",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) error {
-	return c.Status(200).JSON(fiber.Map{
-		"message": "Get book",
-	})
+	id := c.Params("id")
+	var book model.Book
+	if err := database.DB.First(&book, id).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Book not found",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(book)
 }
 
 func CreateBook(c *fiber.Ctx) error {
 	var req bookRequest
 
 	if err := c.BodyParser(&req); err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"message": err.Error(),
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
 		})
 	}
 
@@ -40,21 +51,52 @@ func CreateBook(c *fiber.Ctx) error {
 	res := database.DB.Create(&book)
 
 	if res.Error != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"message": res.Error.Error(),
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": res.Error.Error(),
 		})
 	}
-	return c.Status(201).JSON(fiber.Map{
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "Book created",
 	})
 }
 
 func UpdateBook(c *fiber.Ctx) error {
-	return c.Status(200).JSON(fiber.Map{
-		"message": "Book created",
+	id := c.Params("id")
+	var req bookRequest
+
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	var book model.Book
+	if err := database.DB.First(&book, id).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Book not found",
+		})
+	}
+
+	book.Title = req.Title
+	book.Author = req.Author
+
+	if err := database.DB.Save(&book).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Error updating book",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Book updated",
 	})
 }
 
 func DeleteBook(c *fiber.Ctx) error {
-	return c.Status(204).JSON(fiber.Map{})
+	id := c.Params("id")
+	if err := database.DB.Delete(&model.Book{}, id).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Error deleting book",
+		})
+	}
+	return c.Status(fiber.StatusNoContent).JSON(fiber.Map{})
 }
